5856dda473d87372dc0dc4ac920034da6ddb52f9
Bassel Bitar <91824524+basselbitar@users.noreply.github.com>
2024-03-30 19:14:30 +0000

    Generating Upgrades Placholders in random positions
    In the Host they're getting recolored, whereas for the client they stay white for now

--- Assets/Scripts/Upgrade.cs
+++ Assets/Scripts/Upgrade.cs
@@ -7,28 +7,38 @@ public class Upgrade : MonoBehaviour
     [SerializeField] private int _id;
     [SerializeField] private string _name;
 
-    enum Type { Buff, Nerf, Neutral};
-    enum Aoe { Self, Other, Both, Field };
+    public enum Type { Buff, Nerf, Neutral};
+    public enum Aoe { Self, Other, Both, Field };
     [SerializeField]
     private Type _type;
     private Aoe _aoe;
     
+    private Vector3 _position;
 
     void Start()
     {
+        
+    }
+
+    public void ColorUpgrade() {
         Color color;
-        if(_type == Type.Buff) {
+        if (_type == Type.Buff) {
             color = new Color(0.36f, 0.75f, 0.4f);
-        } else if(_type == Type.Nerf) { 
+        }
+        else if (_type == Type.Nerf) {
             color = new Color(0.75f, 0.2f, 0.2f);
-        } else {
+        }
+        else {
             color = new Color(0.75f, 0.65f, 0.2f);
         }
         GetComponent<SpriteRenderer>().color = color;
     }
 
-    void Update()
-    {
-        
+    public Type GetType() {
+        return _type;
+    }
+
+    public void SetType(Type type) {
+        _type = type;
     }
 }
--- Assets/Scripts/UpgradeManager.cs
+++ Assets/Scripts/UpgradeManager.cs
@@ -1,18 +1,43 @@
+using Alteruna;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UpgradeManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<Upgrade> Upgrades;
+    private Spawner _spawner;
+
+    private const float MIN_X = -5;
+    private const float MIN_Y = -4;
+    private const float MAX_X = 5;
+    private const float MAX_Y = 4;
+
     void Start()
     {
-        
+        PopulateUpgrades();
+        _spawner = FindObjectOfType<Spawner>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void PopulateUpgrades() {
+        Upgrades = new List<Upgrade>();
     }
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.J)) {
+            SpawnUpgrade();
+        }
+    }
+
+    public void SpawnUpgrade() {
+        Vector3 pos = new Vector3(Random.Range(MIN_X, MAX_X), Random.Range(MIN_Y, MAX_Y), 0f);
+        GameObject upgradeGO = _spawner.Spawn(1, pos, Quaternion.identity, new Vector3(1f, 1f, 1f));
+
+        Upgrade upgrade = upgradeGO.GetComponent<Upgrade>();
+
+        upgrade.SetType((Upgrade.Type) Random.Range(0, 3));
+        upgrade.ColorUpgrade();
+
+    }
+
 }
